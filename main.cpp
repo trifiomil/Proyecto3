09e35cf8eff9102bcc4cb1daeeeea7a1843329b6
JuanCGar <JuanCGar@users.noreply.github.com>
2018-04-30 13:53:00 -0500

    Se agrego los lectores de archivo

--- main.cpp
+++ main.cpp
@@ -9,26 +9,55 @@
 #include <iostream>
 #include<fstream>
 #include<string>
+#include<stdio.h>
+#include<stdlib.h>
 #include "Reserva.h"
 #include "Autos.h"
 #include "Aviones.h"
 
 using namespace std;
 int main() {
-
+//OBJETOS
 ifstream Servicios;
 ifstream Reservaciones;
-string a,b,c,d,e,f,g,h;
+Servicio *Serv[6];
+Reserva Reserva[20];
+//VARIABLES
+//reservacion
+string clave;
+int idCliente;
+//fechas
+int dia,mes,ano;
+//dias duracion
+int dias;
+//servicio
+char Tipo;
+int cantidPersonas,adicional;
+double CostoXdia;
+string descripcion;
+bool chofer,blindado;
+//contadores para los dso archivos de texto
+int contReservaciones = 0, contServicios = 0;
 
+
+Reservaciones.open("Reserva.txt");
 Servicios.open("Servicio.txt");
 if(Servicios.is_open())
 {
     cout << "se abrio "<< endl;
-}else
-    cout << "no se abrio" << endl;
+}
+    while(Reservaciones >>clave>>idCliente>>dia>>mes>>ano>>dias)
+    {
+        //convertir a itnregro
+        Fecha FechaCont(dia,mes,ano);
+        Reserva[contReservaciones].setClave(clave);
+        Reserva[contReservaciones].setID(idCliente);
+        Reserva[contReservaciones].setFechaContrato(FechaCont);
+        Reserva[contReservaciones].setDuracion(dias);
 
-   Servicio *Serv[6];
-   Reserva Reserva[20];
+        contReservaciones++;
+    }
+
 
     Servicios.close();
     return 0;
